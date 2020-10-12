57232e4d202778792d456d027cfb7c3a37d7f5a3
Quentin Vandevorst <56817496+Survive95@users.noreply.github.com>
2020-10-12 20:10:36 +0200

    init

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,164 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
+    <header>
+      <div class="header-content">
+        <h1>
+          <img
+            class="logo"
+            src="../src\assets\Zevent_logo.png"
+            alt="zevent_logo"
+          />
+        </h1>
+        <nav>
+          <a :key="letter" v-for="letter in letters" :href="`#${letter}`">{{letter}}</a>
+        </nav>
+      </div>
+    </header>
+    <main>
+      <div :id="letter" class="main_item" :key="letter" v-for="letter in letters">
+        <ul v-if="streamersData !== null" class="streamers_list">
+          <h3 class="letter">{{letter}}</h3>
+          <Streamer
+            :key="index"
+            v-for="(data, index) in streamersData.live.filter(
+              (w) => w.display.charAt(0) === letter
+            )"
+            :stream="data"
+          ></Streamer>
+        </ul>
+      </div>
+    </main>
   </div>
 </template>
 
+<script>
+import Streamer from "./components/Streamer";
+import axios from "axios";
+
+export default {
+  components: {
+    Streamer,
+  },
+  data() {
+    return {
+      streamersData: null,
+      letters: [
+        "A",
+        "B",
+        "C",
+        "D",
+        "E",
+        "F",
+        "G",
+        "H",
+        "I",
+        "J",
+        "K",
+        "L",
+        "M",
+        "N",
+        "O",
+        "P",
+        "Q",
+        "R",
+        "S",
+        "T",
+        "U",
+        "V",
+        "W",
+        "X",
+        "Y",
+        "Z",
+      ],
+    };
+  },
+  mounted() {
+    setInterval(() => {
+      console.log("Update !!!!");
+      axios.get("https://zevent.fr/api/data.json").then((res) => {
+        this.streamersData = res.data;
+      });
+    }, 60000);
+    axios.get("https://zevent.fr/api/data.json").then((res) => {
+      this.streamersData = res.data;
+    });
+  },
+};
+</script>
+
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+  font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
 }
 
-#nav {
-  padding: 30px;
+body {
+  background-color: #191919;
+}
 
-  a {
-    font-weight: bold;
-    color: #2c3e50;
+header {
+  .header-content {
+    padding: 40px 0;
+    width: 80vw;
+    margin-inline-start: auto;
+    margin-inline-end: auto;
+    display: flex;
+    flex-wrap: wrap;
+    align-items: center;
+    justify-content: space-between;
+    @media (max-width: 460px){
+      align-items: center;
+      justify-content: center;
+    }
+  }
+  .logo {
+    width: 100px;
+  }
+  nav{
+    display: flex;
+    flex-wrap: wrap;
+    margin-left: 20px;
+    @media (max-width: 460px){
+      margin: 0;
+      margin-top: 20px;
+    }
+    a{
+      padding: 10px 10px;
+      border-radius: 5px;
+      color: white;
+      text-decoration: none;
+      &:hover{
+        color: #4bba31;
+      }
+    }
+  }
+}
 
-    &.router-link-exact-active {
-      color: #42b983;
+main {
+  width: 80vw;
+  margin-inline-start: auto;
+  margin-inline-end: auto;
+  .streamers_list {
+    list-style: none;
+  }
+  .main_item{
+    padding: 0 0 20px 0;
+    margin-bottom: 20px;
+    .letter{
+      color: white;
+      margin-bottom: 20px;
+      display: flex;
+      align-items: center;
+      &::after{
+        content: '';
+        width: 100%;
+        height: 1px;
+        background-color: #4bba31;
+        display: block;
+        margin-left: 20px;
+      }
     }
   }
 }
---
+++ src/components/Streamer.vue
@@ -0,0 +1,72 @@
+<template>
+ <a target="_blank" class="list_item" :href="`https://www.twitch.tv/${stream.twitch}`">
+     <div class="user">
+         <div class="user-content">
+             <img class="avatar" :src="stream.profileUrl" alt="">
+             <p class="name">{{stream.display}}</p>
+         </div>
+         <div class="status" :class="{active : stream.online}"></div>
+     </div>
+     <div class="games">
+         <p class="play">{{stream.game}}</p>
+         <p class="viewers">{{stream.viewersAmount.number}}</p>
+     </div>
+ </a>
+</template>
+
+<script>
+export default {
+  props: ["stream"],
+};
+</script>
+
+<style lang='scss'>
+
+.list_item{
+    display: flex;
+    flex-direction: column;
+    background-color: rgb(22, 22, 22);
+    margin-bottom: 20px;
+    border-radius: 10px;
+    text-decoration: none;
+}
+.user{
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    padding: 20px 20px 0 20px;
+}
+.user-content{
+    display: flex;
+    align-items: center;
+}
+.status{
+    width: 15px;
+    height: 15px;
+    background-color: rgb(80, 80, 80);
+    display: block;
+    border-radius: 100px;
+    &.active{
+        background-color: #ff0000;
+    }
+}
+
+.avatar{
+    width: 40px;
+}
+
+.name{
+    margin-left: 20px;
+    color: white;
+}
+
+.games{
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    margin-top: 10px;
+    padding: 10px 20px 20px 20px;
+    color: white;
+}
+
+</style>
